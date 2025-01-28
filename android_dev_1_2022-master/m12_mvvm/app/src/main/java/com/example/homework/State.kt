eafddb2c624c6ec965f89f3b3edd2e55e5beddd9
Gert <30831662+GenaPonoma@users.noreply.github.com>
2025-01-29 00:02:20 +0300

    Delete android_dev_1_2022-master directory

--- android_dev_1_2022-master/m12_mvvm/app/src/main/java/com/example/homework/State.kt
+++
@@ -1,9 +0,0 @@
-package com.example.homework
-
-sealed class State {
-    object Loading : State()
-    object Success : State()
-    object ButtonActive : State()
-    object ButtonNotActive: State()
-
-}
--- android_dev_1_2022-master/m9_quiz_localization/app/src/main/java/com/example/skillbox_hw_quiz/quiz/Question.kt
+++
@@ -1,9 +0,0 @@
-package com.example.skillbox_hw_quiz.quiz
-
-class Question(
-    val question: Int,
-    val answers: List<Int>,
-    val feedback: List<Int>
-) {
-
-}
