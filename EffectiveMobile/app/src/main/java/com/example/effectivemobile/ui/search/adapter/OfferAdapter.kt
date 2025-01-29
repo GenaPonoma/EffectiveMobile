fab35321c591c16fcf8773c4f36f06669096098a
gennadii_ponomarev <g.ponomareow2014@yandex.ru>
2025-01-29 22:56:05 +0300

    final project

---
+++ EffectiveMobile/app/src/main/java/com/example/effectivemobile/ui/search/adapter/OfferAdapter.kt
@@ -0,0 +1,86 @@
+package com.example.effectivemobile.ui.search.adapter
+
+
+import android.content.Intent
+import android.net.Uri.parse
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat.getColor
+import androidx.core.content.ContextCompat.getDrawable
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.effectivemobile.R
+import com.example.effectivemobile.data.model.Offer
+import com.example.effectivemobile.databinding.ItemOfferBinding
+
+class OfferAdapter : ListAdapter<Offer, OfferViewHolder>(OfferDiffCallback()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OfferViewHolder {
+        val binding = ItemOfferBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return OfferViewHolder(binding)
+
+    }
+
+    override fun onBindViewHolder(holder: OfferViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.binding.apply {
+            textBlock.text = item.title
+            textButton.visibility = if (item.button?.text == null) View.INVISIBLE else View.VISIBLE
+            Log.d(" icon.setImageResource(R.drawable.search)", "id: ${item.id}")
+            when (item.id) {
+
+                "near_vacancies" -> {
+
+                    icon.setImageResource(R.drawable.vacation)
+                    icon.background = getDrawable(icon.context, R.drawable.circle)
+                }
+
+                "level_up_resume" -> {
+                    icon.setImageResource(R.drawable.smal)
+                    icon.background = getDrawable(icon.context, R.drawable.circle)
+                }
+
+                "temporary_job" -> {
+                    icon.setImageResource(R.drawable.vacation)
+                    icon.background = getDrawable(icon.context, R.drawable.circle)
+                }
+
+                else -> {
+                    icon.background = getDrawable(icon.context, R.drawable.circle_blue)
+                }
+
+
+            }
+            root.setOnClickListener {
+                // Проверяем, что ссылка не пустая и корректная
+                if (!item.link.isNullOrEmpty()) {
+                    try {
+                        // Создаем Intent для открытия ссылки в браузере
+                        val intent = Intent(Intent.ACTION_VIEW, parse(item.link))
+                        // Запускаем Activity с этим Intent'ом
+                        it.context.startActivity(intent)
+                    } catch (e: Exception) {
+                        // Обрабатываем возможные ошибки, например, если ссылка некорректна
+                        Log.e("Adapter", "Error opening link: ${e.message}")
+                    }
+                }
+            }
+        }
+    }
+
+
+}
+
+private class OfferDiffCallback : DiffUtil.ItemCallback<Offer>() {
+    override fun areItemsTheSame(oldItem: Offer, newItem: Offer): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Offer, newItem: Offer): Boolean {
+        return oldItem == newItem
+    }
+}
+
+class OfferViewHolder(val binding: ItemOfferBinding) : RecyclerView.ViewHolder(binding.root)
---
+++ EffectiveMobile/app/src/main/java/com/example/effectivemobile/ui/search/adapter/VacancyAdapter.kt
@@ -0,0 +1,85 @@
+package com.example.effectivemobile.ui.search.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.effectivemobile.R
+import com.example.effectivemobile.data.model.Vacancy
+import com.example.effectivemobile.databinding.RecyclerVacanciesViewBinding
+
+class VacancyAdapter(private val onItemClicked: (String) -> Unit) :
+    ListAdapter<Vacancy, VacancyViewHolder>(VacancyMatchingDiffCallback()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VacancyViewHolder {
+        val binding =
+            RecyclerVacanciesViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return VacancyViewHolder(binding)
+    }
+
+
+    override fun onBindViewHolder(holder: VacancyViewHolder, position: Int) {
+        val item = getItem(position)
+        with(holder) {
+            binding.apply {
+                iconFavorites.setImageResource(if (item.isFavorite == true) R.drawable.favorit_active else R.drawable.favorites)
+
+                if (item.lookingNumber != null) {
+                    viewsCountLooking.text = root.context.resources.getQuantityString(
+                        R.plurals.count_looking,
+                        item.lookingNumber!!,
+                        item.lookingNumber
+                    )
+                } else {
+                    viewsCountLooking.text = root.context.resources.getQuantityString(
+                        R.plurals.count_looking,
+                        0,
+                        0
+                    )
+                }
+                vacancyTitle.text = item.title
+                location.text = item.address?.town
+                companyName.text = item.company
+                experienceRequired.text = item.experience?.previewText
+                postedDate.text = item.publishedDate
+
+                // Добавляем обработчик кликов на весь элемент карточки
+                root.setOnClickListener {
+                    val navController = Navigation.findNavController(it)
+                    navController.navigate(
+                        R.id.action_NavigationSearch_to_VacancyFragment
+                    )
+                }
+                iconFavorites.setOnClickListener {
+                    if (item.isFavorite == true) {
+                        item.isFavorite = false
+                        iconFavorites.setImageResource(R.drawable.favorites)
+                    } else {
+                        onItemClicked(item.id.toString())
+                        iconFavorites.setImageResource(R.drawable.favorit_active)
+                        item.isFavorite = true
+                    }
+
+                }
+
+            }
+
+        }
+    }
+
+
+}
+
+private class VacancyMatchingDiffCallback : DiffUtil.ItemCallback<Vacancy>() {
+    override fun areItemsTheSame(oldItem: Vacancy, newItem: Vacancy): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Vacancy, newItem: Vacancy): Boolean {
+        return oldItem == newItem
+    }
+}
+
+class VacancyViewHolder(val binding: RecyclerVacanciesViewBinding) :
+    RecyclerView.ViewHolder(binding.root)
